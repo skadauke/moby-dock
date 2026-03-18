043afe30e03f96b9b534c9e37ebec92c82280af4
Stephan Kadauke <skadauke@gmail.com>
2026-03-18 05:55:03 -0400

    fix: set search_path on trigger functions (Supabase security lint)

--- supabase/migrations/00000000000000_initial_schema.sql
+++ supabase/migrations/00000000000000_initial_schema.sql
@@ -54,7 +54,8 @@ BEGIN
   NEW.updated_at = NOW();
   RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql
+SET search_path = public;
 
 DROP TRIGGER IF EXISTS trigger_tasks_updated_at ON tasks;
 CREATE TRIGGER trigger_tasks_updated_at
